0a844370b1fb384af3582353ae2d82489c8f6816
zhouzhili <zhouzhili@360.cn>
2019-05-05 16:12:13 +0800

    修复模型加载

--- webglCodes/LoadGLTF/app.js
+++ webglCodes/LoadGLTF/app.js
@@ -50,26 +50,3 @@ const updateAnimate = () => {
 }
 
 updateAnimate()
-
-// 射线拾取
-// var raycaster = new THREE.Raycaster()
-// var mouse = new THREE.Vector2()
-// function onMouseClick(evt) {
-//   mouse.x = (evt.clientX / window.innerWidth) * 2 - 1
-//   mouse.y = (evt.clientY / window.innerHeight) * 2 - 1
-
-//   raycaster.setFromCamera(mouse, ctx.camera)
-
-//   const intersects = raycaster.intersectObjects(ctx.scene.children)
-//   const plan = intersects.find(m => m.object.name === 'plan')
-
-//   if (plan) {
-//     const point = plan.point
-//     const kGirls = ctx.scene.children.find(m => m.name === 'kGirls')
-//     kGirls.position.x = -1 * point.x
-//     kGirls.position.z = point.z
-//   }
-
-// }
-
-// window.addEventListener('click', onMouseClick, false)

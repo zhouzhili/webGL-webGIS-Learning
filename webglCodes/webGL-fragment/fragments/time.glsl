34bbf02a179db3ff9bfd0432af54c3df82f30051
zhouzhili <zhouzhili@360.cn>
2019-07-22 10:12:40 +0800

    动态圆圈

--- webglCodes/webGL-fragment/fragments/time.glsl
+++ webglCodes/webGL-fragment/fragments/time.glsl
@@ -3,7 +3,13 @@ precision mediump float;
 #endif
 
 uniform float uTime;
+uniform vec2 uResolution;
 
 void main(){
-  gl_FragColor=vec4(abs(sin(uTime)),0.,0.,1.);
+  vec2 coord=gl_FragCoord.xy/uResolution;
+  vec2 center=vec2(.5);
+  float dis=distance(coord,center);
+  float rad=mod(uTime,10.)*.01;
+  gl_FragColor.rgb=step(rad,dis)*vec3(1.);
+  gl_FragColor.a=1.;
 }

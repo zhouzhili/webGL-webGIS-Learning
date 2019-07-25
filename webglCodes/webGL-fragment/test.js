ab8d88f5fd1cdb2bc1ed01f6861e9cc03d0c54ba
zhouzhili <zhouzhili@360.cn>
2019-07-25 15:15:01 +0800

    delete unuse file

--- webglCodes/webGL-fragment/test.js
+++
@@ -1,8 +0,0 @@
-for (let i = 0; i < 0.2; i += 1 / 512) {
-  for (let j = 0; j < 0.2; j += 1 / 512) {
-    var st = Math.sqrt(Math.pow(i - 0.5, 2) + Math.pow(j - 0.5, 2))
-    st = st * 10
-    var d = st - Math.floor(st)
-    console.log(d)
-  }
-}

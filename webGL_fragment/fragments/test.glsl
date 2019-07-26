e0f315afc6633ec0432dedad904921fbf3f18765
zhouzhili <zhouzhili@360.cn>
2019-07-26 15:59:02 +0800

    fixed include error

--- webGL_fragment/fragments/test.glsl
+++ webGL_fragment/fragments/test.glsl
@@ -4,8 +4,10 @@ precision mediump float;
 
 #include <shape.glsl>
 
+uniform vec2 uResolution;
+
 void main(){
-  vec2 st=gl_FragCoord.xy;
+  vec2 st=gl_FragCoord.xy/uResolution;
   vec3 line =sdLine(st,vec2(0.5),vec2(0.9),vec3(1.0));
   gl_FragColor.rgb=line;
   gl_FragColor.a=1.;

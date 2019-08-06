f0f91c7fadddce2b671b8836a0c05e534fb32823
zhouzhili <zhouzhili@360.cn>
2019-08-06 16:20:59 +0800

    删除多余文件

--- webglCodes/webgl-drawLine/app.js
+++
@@ -1,9 +0,0 @@
-import { GRender } from '@/utils/fragmentDraw'
-import fragmentShader from './fragment.glsl'
-
-const gRender = new GRender({
-  canvas: document.getElementById('gl-canvas'),
-  fragmentShader: fragmentShader
-})
-
-gRender.render()

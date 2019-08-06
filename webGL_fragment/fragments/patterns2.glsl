0b9e2fc4e0e6dfed0dd353f0ac11341312ac37ca
zhouzhili <zhouzhili@360.cn>
2019-08-06 14:10:29 +0800

    格式化文件

--- webGL_fragment/fragments/patterns2.glsl
+++ webGL_fragment/fragments/patterns2.glsl
@@ -15,11 +15,11 @@ vec2 grid2(in vec2 st,in float row,in float col){
   float m=mod(uTime,4.);
   
   // 奇数行偏移正的,偶数偏移负的
-  p.x+=step(0.,2.01-m)*step(1.,mod(p.y,2.))*m*1.;
-  p.x-=step(0.,2.01-m)*(1.-step(1.,mod(p.y,2.)))*m*1.;
+  p.x+=step(0.,2.01-m)*step(1.,mod(p.y,2.))*m;
+  p.x-=step(0.,2.01-m)*(1.-step(1.,mod(p.y,2.)))*m;
   
-  p.y+=step(0.,m-2.01)*step(1.,mod(p.x,2.))*m*1.;
-  p.y-=step(0.,m-2.01)*(1.-step(1.,mod(p.x,2.)))*m*1.;
+  p.y+=step(0.,m-2.01)*step(1.,mod(p.x,2.))*m;
+  p.y-=step(0.,m-2.01)*(1.-step(1.,mod(p.x,2.)))*m;
   
   return fract(p);
 }

473b686fbfd0396232bad1fe0e097608db838b29
zhouzhili <zhouzhili@360.cn>
2019-08-12 14:25:26 +0800

    修改为2d随机

--- webGL_fragment/fragments/random.glsl
+++ webGL_fragment/fragments/random.glsl
@@ -3,11 +3,6 @@ precision mediump float;
 #endif
 
 uniform vec2 uResolution;
-uniform float uTime;
-uniform sampler2D uSampler;
-
-#include <lib/util.glsl>
-#include <lib/shape.glsl>
 
 float rand(vec2 st) {
   return fract(sin(dot(st, vec2(12.9898, 78.233))) * 43758.5453);
@@ -15,9 +10,7 @@ float rand(vec2 st) {
 
 void main(){
 	vec2 st=gl_FragCoord.xy/uResolution;
-	float dis = sRect(st,vec2(0.5),vec2(0.2));
-	vec3 color=texture2D(uSampler,st.xy).rgb;
 
-	gl_FragColor.rgb=fill(dis,color);
+	gl_FragColor.rgb=rand(st)*vec3(1.0);
 	gl_FragColor.a=1.;
 }
